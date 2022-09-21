939c37ddf008cec445c703b35e4a06edda7a1caf
iwansafr <iwansafr@gmail.com>
2022-09-21 17:32:39 +0700

    home template

---
+++ resources/views/dashboard/partial/meta.blade.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>@yield('title') - {{ env('APP_NAME') }}</title>
+
+<link rel="stylesheet" href="/dist/assets/css/main/app.css">
+<link rel="stylesheet" href="/dist/assets/css/main/app-dark.css">
+
+<link rel="shortcut icon" href="/dist/assets/images/logo/favicon.svg" type="image/x-icon">
+<link rel="shortcut icon" href="/dist/assets/images/logo/favicon.png" type="image/png">
