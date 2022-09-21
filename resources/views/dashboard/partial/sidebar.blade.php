0fe9d73deb67156c318a9bfe116672e19f9e6492
esoftgreat <iwansafr@gmail.com>
2022-09-21 21:39:13 +0700

    meta

---
+++ resources/views/dashboard/partial/sidebar.blade.php
@@ -0,0 +1,23 @@
+<ul class="menu">
+    <li class="sidebar-title">Menu</li>
+
+    <li class="sidebar-item  ">
+        <a href="index.html" class='sidebar-link'>
+            <i class="bi bi-grid-fill"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <li class="sidebar-item  has-sub">
+        <a href="#" class='sidebar-link'>
+            <i class="bi bi-stack"></i>
+            <span>Components</span>
+        </a>
+        <ul class="submenu ">
+            <li class="submenu-item ">
+                
+                <a href="component-alert.html"> <i class="bi bi-stack"></i> Alert</a>
+            </li>
+        </ul>
+    </li>
+</ul>
