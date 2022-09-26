16d86e081389abff617bbe5683e49b07410a1995
iwansafr <iwansafr@gmail.com>
2022-09-26 12:04:44 +0700

    prepare for content and category

---
+++ database/migrations/2022_09_26_050223_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parent')->nullable();
+            $table->foreign('parent')
+                ->references('id')
+                ->on('menus')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->tinyInteger('publish')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
