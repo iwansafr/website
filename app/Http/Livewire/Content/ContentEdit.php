6598a07c45ca4eeac5f3776d925a229bed1010b1
esoftgreat <iwansafr@gmail.com>
2022-10-04 19:57:39 +0700

    prepare content edit with categories option

--- app/Http/Livewire/Content/ContentEdit.php
+++ app/Http/Livewire/Content/ContentEdit.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Livewire\Content;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class ContentEdit extends Component
 {
     public $title;
     public $content;
+    public $categoryList;
     protected $rules = [
         'title' => 'required'
     ];
+
+    public function mount()
+    {
+        $this->categoryList = Category::with('parentCategory')->limit(12)->get();
+    }
+
     public function render()
     {
         return view('livewire.content.content-edit');
--- resources/views/livewire/content/content-edit.blade.php
+++ resources/views/livewire/content/content-edit.blade.php
@@ -8,11 +8,35 @@
                 @if (session()->has('msg'))
                     <div class="alert alert-success"><i class="bi bi-file-excel"></i> {{ session('msg') }}</div>
                 @endif
-                
+
                 <x-input name="title" label="Title"></x-input>
+                <x-input name="searchCategory" label="Category"></x-input>
+                @if ($categoryList->isNotEmpty())
+                    <div class="table-wrapper-scroll-y my-custom-scrollbar">
+                        <table class="table">
+                            @foreach ($categoryList as $item)
+                                <tr>
+                                    <td>
+                                        {{ !empty($item->parentCategory->title) ? $item->parentCategory->title.' -> ' : '' }}
+                                        {{ $item->title }}
+                                    </td>
+                                    <td class="float-end">
+                                        <span
+                                            wire:click="setCategories('{{ $item->id }}')"class="btn btn-sm btn-primary">{{ __('Pilih') }}</span>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                @endif
+                @if (!empty($categories))
+                    @foreach ($categories as $key => $value)
+                        <span class="btn btn-sm btn-info">{{ $value }}</span>
+                    @endforeach
+                @endif
                 <div wire:ignore>
                     <div id="full" wire:model="content">
-                        
+
                     </div>
                 </div>
             </div>
@@ -21,7 +45,7 @@
                     <button class="btn btn-primary ml-1 disabled">
                         <i class="bi bi-clock-history"></i>
                         <span>{{ __('Sedang Mengirim Data') }} ...</span>
-                    </button>    
+                    </button>
                 </div>
                 <div wire:loading.remove wire:target="save">
                     <button type="submit" class="btn btn-primary ml-1">
