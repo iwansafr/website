2f40997fe16251514717b7c37d5ca8137615b0f5
esoftgreat <iwansafr@gmail.com>
2022-11-06 14:10:07 +0700

    undo ui todolist.js

--- public/dist/assets/js/pages/ui-todolist.js
+++ public/dist/assets/js/pages/ui-todolist.js
@@ -1,5 +1,1 @@
-
-dragula([document.getElementById("widget-todo-list")], { moves: function (e, a, t,s) {
-    console.log(a)
-    return t.classList.contains("cursor-move") 
-} })
+dragula([document.getElementById("widget-todo-list")], { moves: function (e, a, t,s) { return t.classList.contains("cursor-move") } })
