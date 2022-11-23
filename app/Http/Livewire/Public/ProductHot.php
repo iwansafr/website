3113ba1b4a6775381f4296b6797e4cf8c5cb7433
iwansafr <iwansafr@gmail.com>
2022-11-23 17:46:51 +0700

    hot product on public

---
+++ app/Http/Livewire/Public/ProductHot.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Public;
+
+use Livewire\Component;
+
+class ProductHot extends Component
+{
+    public function render()
+    {
+        return view('livewire.public.product-hot');
+    }
+}
